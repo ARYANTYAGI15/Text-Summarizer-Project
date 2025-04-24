f78ffae4a5b8e634588c8625c23c016ef1c9f7a7
ARYANTYAGI15 <aryan.india1515@gmail.com>
2025-04-25 02:15:36 +0530

    Data Validation Notebook Done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Text_Summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from Text_Summarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from Text_Summarizer.logging import logging
 
 
@@ -12,3 +13,15 @@ try:
 except Exception as e:
     logging.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
