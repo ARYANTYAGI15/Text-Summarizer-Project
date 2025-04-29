dd19e02f230fe6aee1588926c265991e32b9f43b
ARYANTYAGI15 <aryan.india1515@gmail.com>
2025-04-29 09:49:27 +0530

    Model Evaluation Done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Text_Summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainin
 from Text_Summarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from Text_Summarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from Text_Summarizer.pipeline.stage04_model_train import ModelTrainingPipeline
+from Text_Summarizer.pipeline.stage05_model_evaluation import ModelEvaluationTrainingPipeline
 from Text_Summarizer.logging import logging
 
 
@@ -45,6 +46,17 @@ STAGE_NAME = "Model Training Stage"
 try:
     logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
     model_train = ModelTrainingPipeline()
+    model_train.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+            logging.exception(e)
+            raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    model_train = ModelEvaluationTrainingPipeline()
     model_train.main()
     logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
